8984e6f1f90036c879716b2391d65f63e5c29042
Shizool <35669764+Shizool@users.noreply.github.com>
2018-02-25 03:18:24 +0100

    Update zad5.c

--- zad5.c
+++ zad5.c
@@ -35,3 +35,23 @@ int main(int argc, char *argv[])
     fclose(plik_wy);
     return 0;
 }
+
+/*
+Opracować program wczytujący z pliku dane tekstowe i zapisujący do drugiego pliku tylko liczby
+zawierając cyfry z zakresu podanego w wierszu poleceń podczas uruchomienia programu. Można
+założyć, że dane będą poprawne. Przykładowo uruchomienie programu:
+$ prog 138 plik_we.txt plik_wy.txt
+oraz zawartości pliku plik_we.txt:
+2233
+44
+222
+112123
+4997
+556
+777
+8
+spowoduje zapisanie w plik_wy.txt tylko liczb zawierających cyfry 1, 3 i 8:
+2233
+112123
+8
+*/
